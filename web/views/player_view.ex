517c334bb37e1737d2658919b80f746c78f37a4b
tlrobrn <obrien.taylorm@gmail.com>
2015-10-23 20:07:28 -0500

    Add values as data attributes

--- web/views/player_view.ex
+++ web/views/player_view.ex
@@ -5,13 +5,14 @@ defmodule PickleballLeague.PlayerView do
 
   def sorted_players_with_eprs(players, opponent_eprs) do
     players
-    |> Enum.sort(fn (p1, p2) -> length(p1.earned_points_ratios) >= length(p2.earned_points_ratios) end)
-    |> Enum.sort(fn (p1, p2) -> opponent_eprs[p1] >= opponent_eprs[p2] end)
-    |> Enum.map(fn player -> {player, calculate_epr(player)} end)
-    |> Enum.sort(fn ({_p1, epr1}, {_p2, epr2}) -> epr1 >= epr2 end)
+    |> Enum.sort_by(&length(&1.earned_points_ratios), &>=/2)
+    |> Enum.sort_by(&String.to_float(opponent_eprs[&1]), &>=/2)
+    |> Enum.map(&({&1, calculate_epr(&1)}))
+    |> Enum.sort_by(fn {_p, epr} -> epr end, &>=/2)
   end
 
   def format_epr(@no_games_value), do: "-----"
+  def format_epr(nil), do: "-----"
   def format_epr(epr), do: Float.to_string(epr, decimals: 3)
 
   defp calculate_epr(player) do
